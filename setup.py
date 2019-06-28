a8ca85b8d3036e5addb98c5bede16f04c895ce4a
jgehringUCB <jase.gehring@gmail.com>
2019-06-27 19:32:34 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='kitefeaturebarcoding',
       version='0.1',
       description='Fast and accurate quantification of single-cell Feature Barcoding experiments. This package prepares mismatch fasta and mismatch t2g files from a Python dictionary of Feature Barcode name and sequence key-value pairs.',
-      url='http://github.com/pachterlab/kite',
+      url='http://github.com/jgehringUCB/kite',
       author='Eduardo da Veiga Beltrame, Jase Gehring',
       author_email='beltrame@caltech.edu, jgehring@caltech.edu',
       license='BSD-2',
